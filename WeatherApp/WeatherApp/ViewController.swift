daa1f3121259635b83dd6d20f9d73864ee18ede9
denis <bluesky171@mail.ru>
2025-02-19 11:48:49 +0300

    create project + add pods

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Денис Ефименков on 19.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
